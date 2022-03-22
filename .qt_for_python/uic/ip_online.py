a751b93c582f74f95f9a7ec4e1ad8d7e13103da3
wsy <wsy8718@163.com>
2022-03-22 23:25:06 +0800

    第一版 用Pyinstaller生成应用程序

--- .qt_for_python/uic/ip_online.py
+++ .qt_for_python/uic/ip_online.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'd:\_python\xdkj_test\project\ui\ip_online.ui'
+# Form implementation generated from reading ui file 'd:\_python\xdkj_test\dist\main\project\ui\ip_online.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.6
 #
