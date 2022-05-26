865e7296c2f87295de1a122c8f629eb3b15db485
wsy <wsy8718@163.com>
2022-05-26 22:30:48 +0800

    run init

--- project/ipOnline/pack/standard_model.py
+++ project/ipOnline/pack/standard_model.py
@@ -1,6 +1,8 @@
 # coding:utf8
 
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from project.ipOnline.ui.test_view_standard import Ui_Form
+from PyQt5.QtWidgets import QWidget, QApplication
 
 class MyStandardModel(QStandardItemModel):
     def __init__(self):
@@ -29,3 +31,36 @@ class MyStandardModel(QStandardItemModel):
 def factory_standard_item_model():
     obj = MyStandardModel()
     return obj
+
+
+
+
+class MyUi(QWidget, Ui_Form):
+    def __init__(self):
+        """ """
+        super(MyUi, self).__init__()
+        self.setupUi(self)
+        self.init()
+
+    def init(self):
+        self.model = factory_standard_item_model()
+        self.tableView.setModel(self.model)
+
+    def on_clicked_btn(self):
+        print('nihao')
+        item = QStandardItem()
+        lit_str = ['name1', 'name2' ]
+        list_item = [QStandardItem(item_str) for item_str in lit_str]
+        # self.model.appendRow(list_item)
+        self.model.insertRow(2, list_item)
+
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    dia = MyUi()
+    dia.show()
+    sys.exit(app.exec())
+
+
+    pass
