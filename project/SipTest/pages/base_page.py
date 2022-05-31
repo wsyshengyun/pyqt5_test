38c2b2e043781609983e8bdd26a99818d29f2744
wsy <wsy8718@163.com>
2022-05-31 23:23:46 +0800

    应用page_object-pattern模式, 即POM

---
+++ project/SipTest/pages/base_page.py
@@ -0,0 +1,45 @@
+#  Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+#  Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+#  Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+#  Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+#  Vestibulum commodo. Ut rhoncus gravida arcu.
+
+# coding:utf8
+
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+
+class BasePage(object):
+    def __init__(self, driver, base_url=''):
+        """ """
+        self.base_url = base_url
+        self.driver = driver
+        self.timeout = 30
+
+    def find_element(self, *locator):
+        return self.driver.find_element(*locator)
+
+    def open(self, url):
+        url = self.base_url + url
+        self.driver.get(url)
+
+    def get_title(self):
+        return self.driver.title
+
+    def get_url(self):
+        return self.driver.current_url
+
+    def hover(self, *locator):
+        element = self.find_element(*locator)
+        hover = ActionChains(self.driver).move_to_element(element)
+        hover.perform()
+
+    def wait_element(self, *locator):
+        try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(locator))
+        except TimeoutException:
+            print('\n * element not found within given time! --> %s' %  (locator[1]))
+            self.driver.quit()
---
+++ project/SipTest/utils/locators.py
@@ -0,0 +1,19 @@
+#  Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+#  Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+#  Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+#  Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+#  Vestibulum commodo. Ut rhoncus gravida arcu.
+
+# coding:utf8
+
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators(object):
+    LOGO = (By.ID, 'nav-logo')
+    LOGO = (By.ID, 'nav-logo')
+    SEARCH_LIST = (By.CSS_SELECTOR, 'div[data-component-type="s-search-result"]')
+
+
+class LoginPageLocators(object):
+    password = (By.ID, '')
