04be4b2d1cc232d3456e036e6188f3529b424655
wsy <wsy8718@163.com>
2022-06-03 12:10:20 +0800

    add sip locators

--- project/SipTest/utils/locators.py
+++ project/SipTest/utils/locators.py
@@ -18,6 +18,28 @@ class LoginPageLocators(object):
     password = (By.ID, '')
 
 
+class SipMainLocators(object):
+    update_page = (By.LINK_TEXT, '升级')   # 页按钮 - 升级
+    update_select_file = (By.CLASS_NAME, 'btninput_l lit_btninput')  # 选择文件
+    update_submit = (By.CLASS_NAME, 'WebUpdate')    # 升级按钮
+
+    config_page = (By.LINK_TEXT, '系统配置')  # 系统配置 - 页按钮
+    config_select_file = (By.XPATH, '//*[@id="ImportBody"]/table/tbody/tr/td[5]/input')  # 导入配置 按钮
+    config_submit = (By.XPATH, '//*[@id="System"]')  # 选择文件 按钮
+
+    dj_page = (By.ID, 'XSTR_HLP_SETTING_PHONE')  # 对讲设置 竖向 页按钮
+    mt_page = (By.LINK_TEXT, '媒体设置')  # 媒体设置 页按钮
+    camera_page = (By.LINK_TEXT, '相机设置')  # 相机设置 页按钮
+
+    mt_bro = (By.ID, 'XSTR_LBL_GEN_MEDIA')  # 媒体设置 之 >>
+
+    camera_outer_box = (By.ID, 'Native_Camera_Connect_Mode_RW')  # 相机设置 外接 ComBox
+    camera_btn_submit = (By.XPATH, '//*[@id="ipcMode"]/div[2]/input[2]')  # 相机设置 外接 提交
+
+
+
+
+
 class BaiduPageLocators(object):
     # input = (By.ID, 'kw')
     logo = (By.ID, 's_mp')
