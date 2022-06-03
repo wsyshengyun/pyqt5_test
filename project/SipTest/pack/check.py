39c066196a81263be761317b6a4086e286d04de6
wsy <wsy8718@163.com>
2022-06-03 13:29:17 +0800

    check pc environment

---
+++ project/SipTest/pack/check.py
@@ -0,0 +1,12 @@
+#  Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+#  Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+#  Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+#  Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+#  Vestibulum commodo. Ut rhoncus gravida arcu.
+
+# coding:utf8
+"""
+检查本机PC的ip字段,是否含有要配置的IP字段
+如果没有,添加一个字段
+
+"""
