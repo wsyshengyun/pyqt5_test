e6e987332390fc57dab70a69e95fa11b819460a9
wsy <wsy8718@163.com>
2022-06-05 00:41:01 +0800

    功能：通过UI界面，修改IP数据文件 上一个IP， 下一个IP， 保存

--- project/SipTest/pack/modify_sip_config.py
+++ project/SipTest/pack/modify_sip_config.py
@@ -19,10 +19,10 @@ import re
 from project.ipOnline.pack._currency import IP
 
 
-class ConfigFile(object):
-    restr = r".*WAN IP             :(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
+class ManageConfigFile(object):
+    __common_ip = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+    re_ip = r".*WAN IP\s+:({})".format(__common_ip)
     re_ip_tail = r".*WAN IP\s+:\d{1,3}\.\d{1,3}\.\d{1,3}\.(\d{1,3})"
-
     re_number = r".*SIP1 Phone Number\s.*:8(\d{1,3})"
     re_name = r".*SIP1 Display Name\s.*:8(\d{1,3})"
     re_user = r".*SIP1 Register User\s.*:8(\d{1,3})"
@@ -32,81 +32,96 @@ class ConfigFile(object):
     re_name_ = r"(.*SIP1 Display Name\s.*:8)\d{1,3}"
     re_number_ = r"(.*SIP1 Phone Number\s.*:8)\d{1,3}"
 
-    re_ip = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
-    re_gateway = r"(.*WAN Gateway\s+:)" + re_ip
-    re_sip_service = r"(.*SIP1 Register Addr\s+:)" + re_ip
+    re_gateway = r".*WAN Gateway\s+:({})".format(__common_ip)
+    re_sip_service = r".*SIP1 Register Addr\s+:({})".format(__common_ip)
+    re_gateway_ = r"(.*WAN Gateway\s+:)" + __common_ip
+    re_sip_service_ = r"(.*SIP1 Register Addr\s+:)" + __common_ip
 
     def __init__(self, path=r"D:\_python\xdkj_test\project\SipTest\sip_config.txt"):
         """ """
         self.path = path
         self.ip = None
-        self.content = None
+        self.__content = self.readfile()
         self.get_currip()
 
-    def replace_ip(self, value):
-        tsr = re.sub(self.re_number_, lambda m: m.group(1) + value, self.content)
-        tsr = re.sub(self.re_name_, lambda m: m.group(1) + value, tsr)
-        tsr = re.sub(self.re_user_, lambda m: m.group(1) + value, tsr)
-        tsr = re.sub(self.re_ip_tail_, lambda m: m.group(1) + value, tsr)
+    @property
+    def content(self):
+        return self.__content
+
+    @content.setter
+    def content(self, value):
+        self.__content = value
+
+    def update_ip_tail(self, ip_tail: str):
+        tsr = re.sub(self.re_number_, lambda m: m.group(1) + ip_tail, self.content)
+        tsr = re.sub(self.re_name_, lambda m: m.group(1) + ip_tail, tsr)
+        tsr = re.sub(self.re_user_, lambda m: m.group(1) + ip_tail, tsr)
+        tsr = re.sub(self.re_ip_tail_, lambda m: m.group(1) + ip_tail, tsr)
         self.content = tsr
-        self.write()
         return tsr
 
     def write(self):
+        if self.content == self.readfile():
+            return
+
         with open(self.path, 'w', encoding='utf8') as f:
             f.write(self.content)
 
     def get_currip(self):
-        self.ip = None
-        content = self.readfile()
-        matter = re.search(self.restr, content)
+        matter = re.search(self.re_ip, self.content)
         if matter:
-            return matter.group(1)
+            self.ip = matter.group(1)
+            return self.ip
 
     def readfile(self):
         with open(self.path, 'r', encoding='utf8') as f:
             line_list = f.readlines()
-            content = ''.join(line_list)
-            # print(content)
-            self.content = content
-            return content
+            return ''.join(line_list)
 
-    def set_ip_add1(self):
-        """
-        IP的末端值+1
-        """
-
-        pass
-
-    def set_ip_value(self, tail_num):
-        """
-        设置iP的末端到某个值
-        """
-        pass
-
-    def set_base(self):
-        """
-        设置本次配置文件的基本参数
-        """
-        pass
+    def check_ip(self, ip: str):
+        if not IP(self.ip).is_equal_section(ip):
+            raise ValueError("IP不在一个网段")
 
     def replace_gateway(self, ip_gateway):
-        tsr = re.sub(self.re_gateway, lambda m: m.group(1) + ip_gateway, self.content)
-        self.content = tsr
-        return tsr
-        pass
+        self.check_ip(ip_gateway)
+        self.content = re.sub(self.re_gateway_, lambda m: m.group(1) + ip_gateway, self.content)
 
     def replace_sip_service(self, ip_sip_service):
-        tsr = re.sub(self.re_sip_service, lambda m: m.group(1) + ip_sip_service, self.content)
-        self.content = tsr
-        return tsr
+        self.check_ip(ip_sip_service)
+        self.content = re.sub(self.re_sip_service_, lambda m: m.group(1) + ip_sip_service, self.content)
+
+    def get_config_info(self):
+        """
+
+        """
+        info_dict = {'配置文件IP为：': self.ip}
+
+        match = re.search(self.re_gateway, self.content)
+        if match:
+            info_dict['默认网关为：'] = match.group(1)
+
+        match = re.search(self.re_sip_service, self.content)
+        if match:
+            info_dict['SIP服务器地址为:'] = match.group(1)
+
+        return '\n'.join([' '.join([key, value]) for key, value in info_dict.items()])
+
+    def update_ip(self, ip):
+        """
+
+        """
+        # 更新ip配置文件的当前ip值
+        # 更新电话号码3个
+        self.check_ip(ip)
+        tail = IP(ip).tail()
+        self.update_ip_tail(tail)
+        self.ip = ip
 
 
 def main():
     """ """
-    obj = ConfigFile()
-    # obj.readfile()
-    print(obj.get_currip())
+    obj = ManageConfigFile()
+    print(obj.get_config_info())
     pass
 
 
--- project/SipTest/ui/widget_set.py
+++ project/SipTest/ui/widget_set.py
@@ -8,6 +8,7 @@
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QFileDialog
 from project.SipTest.ui.ui_set import Ui_Form
 from project.SipTest.pack.data import get_config, set_config, config_dict
+from project.SipTest.pack.modify_sip_config import ManageConfigFile
 
 
 class WidgetSet(QWidget, Ui_Form):
@@ -35,9 +36,32 @@ class WidgetSet(QWidget, Ui_Form):
         """
         # 打开选择文件对话框
         # 把路径定位到桌面
-        print("btn_open")
-        dialog = QFileDialog()
-        dialog.exec()
+        fd = QFileDialog(
+            # self, "选择SIP配置文件", "./", "ALL(*, *);;Images(*.png *.jpg);;Python文件(*.py)"
+            self, "选择SIP配置文件", "./", "txt文件(*.txt);;Images(*.png *.jpg);;Python文件(*.py)"
+        )
+        # -------接收模式-----------
+        fd.setAcceptMode(QFileDialog.AcceptOpen)  # 打开
+        # fd.setAcceptMode(QFileDialog.AcceptSave)  # 保存
+        # ------默认后缀名----------
+        fd.setDefaultSuffix("txt")
+        # ------设置文件模式--------
+        fd.setFileMode(QFileDialog.AnyFile)  # 任何文件
+        # fd.setFileMode(QFileDialog.ExistingFile)  # 存在的文件（单个）
+        # fd.setFileMode(QFileDialog.ExistingFiles)  # 存在的文件（多个）
+        # fd.setFileMode(QFileDialog.Directory)  # 目录
+        fd.open()
+        fd.fileSelected[str].connect(self.open_file)
+
+    def open_file(self, path_file):
+        self.line_cfg_path.setText(path_file)
+        # 保存路径到配置文件里面
+        self.config_dict['path_config'] = path_file
+        set_config()
+
+        # 读取文件内容， 分析文件内容，把结果显示在text框里面
+        manage = ManageConfigFile(path_file)
+        self.textEdit_2.setText(manage.get_config_info())
 
     def on_btn_ok(self):
         """
@@ -45,10 +69,6 @@ class WidgetSet(QWidget, Ui_Form):
         """
         # 保存数据
         # 关闭窗口
-
-
-
-
 
 
 if __name__ == '__main__':
